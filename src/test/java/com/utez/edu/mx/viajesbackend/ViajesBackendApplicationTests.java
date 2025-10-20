7aa31d46b5261f5e021f980baad3f1caab79cb95
Jafetvu <20233tn169@utez.edu.mx>
2025-10-20 09:56:09 -0600

    first commit

---
+++ src/test/java/com/utez/edu/mx/viajesbackend/ViajesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utez.edu.mx.viajesbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViajesBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
